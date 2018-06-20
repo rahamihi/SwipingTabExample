b17ead8bd2a344e5c9cbe66c843034ab19a1e554
unknown <Mamun@Mamun-Or-Rashid.btraccl.com>
2018-06-20 10:34:33 +0600

    Initial commit

---
+++ app/src/main/java/com/example/mamun/swipingtabexample/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.mamun.swipingtabexample;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        tabLayout.addTab(new);
+
+
+
+
+    }
+}
